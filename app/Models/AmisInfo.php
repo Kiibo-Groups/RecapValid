d8ecd5406d550109ec2953092af35094287416c8
Kiibo-Groups <kiibogroups@gmail.com>
2024-05-16 02:16:23 -0700

    limit to report

--- app/Models/AmisInfo.php
+++ app/Models/AmisInfo.php
@@ -90,7 +90,7 @@ class AmisInfo extends Model
             {
                 $query->whereDate('upload_xlsx.created_at','<=',$to);
             }
-        })->orderBy('id','ASC')->get();
+        })->orderBy('id','ASC')->limit(100)->get();
 
         $allData = []; 
 
